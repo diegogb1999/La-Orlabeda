cc851448e6aaecab12f6b6769aa135644f8990aa
diegogb1999 <diegogb1999@gmail.com>
2024-02-11 17:33:33 +0100

    arreglar error consola

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+
+export const environment = {
+    firebase: {
+        apiKey: "KEY_GOES_HERE",
+        authDomain: "DOMAIN_GOES_HERE",
+        databaseURL: "DBURL_GOES_HERE",
+        projectId: "laorlabeda",
+        storageBucket: "STORAGEBUCKET_GOES_HERE",
+        messagingSenderId: "SENDERID_GOES_HERE",
+        appId: "APPID_GOES_HERE"
+    }
+};
--- src/services/firebase.service.ts
+++ src/services/firebase.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { initializeApp, getApp, getApps } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
+import { environment } from 'src/environments/environment';
 import 'firebase/auth';
 
 
@@ -9,15 +10,6 @@ import 'firebase/auth';
 })
 export class FirebaseService {
 
-  private firebaseConfig = {
-    apiKey: "KEY_GOES_HERE",
-    authDomain: "DOMAIN_GOES_HERE",
-    databaseURL: "DBURL_GOES_HERE",
-    projectId: "laorlabeda",
-    storageBucket: "STORAGEBUCKET_GOES_HERE",
-    messagingSenderId: "SENDERID_GOES_HERE",
-    appId: "APPID_GOES_HERE"
-  };
 
   private app = this.initializeFirebase();
 
@@ -26,7 +18,7 @@ export class FirebaseService {
   private initializeFirebase() {
     // Verifica si ya existen instancias de la aplicación de Firebase para evitar errores de inicialización múltiple
     if (!getApps().length) {
-      return initializeApp(this.firebaseConfig);
+      return initializeApp(environment.firebase);
     } else {
       return getApp(); // Si ya está inicializada, retorna la instancia existente
     }
